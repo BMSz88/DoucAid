eef25ca15f022a936dc51afabbeb49b43b56f1e8
Navod Chamil Senarathna <161553002+navodchamil@users.noreply.github.com>
2025-02-12 01:26:55 +0530

    Revert "login page"

--- server/Employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const EmployeeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String }
-});
-
-module.exports = mongoose.model("Employee", EmployeeSchema);
